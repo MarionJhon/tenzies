8d7b0c8ef8efa8fc2631047afe2f731418162369
Marion Jhon Agapay <marionagapay2001@gmail.com>
2025-07-26 22:24:15 +0800

    The generate dice is change to object which is value for randomNum, isHeld, and id. Apply the isHeld props in Die components

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,25 +1,39 @@
-import { useState } from "react";
+import { use, useState } from "react";
+import { nanoid } from "nanoid";
 import Die from "./Die";
 import "../App.css";
 
 function App() {
-
+  //this function is to generate 10 new dice with a random number 1-6
   const generateAllNewDice = () => {
-    return Array.from({ length: 10 }, () => Math.ceil(Math.random() * 6));
+    return Array.from({ length: 10 }, () => ({
+      value: Math.ceil(Math.random() * 6),
+      isHeld: true,
+      id: nanoid(),
+    }));
   };
 
-  const [allNewDice, setAllNewDice] = useState(generateAllNewDice);
+  const [allNewDice, setAllNewDice] = useState(generateAllNewDice());
+  const [dice, setDice] = useState(false)
 
-  const dice = allNewDice.map(newDice => (
-    <Die value={newDice}/>
-  ))
+
+
+  const dices = allNewDice.map((newDiceObj) => (
+    <Die key={newDiceObj.id} value={newDiceObj.value} isHeld={newDiceObj.isHeld} />
+  ));
+
+  //this function is to update the setAllNewDice state, it re-roll new dice
+  const reRoll = () => {
+    setAllNewDice(generateAllNewDice);
+  };
 
   return (
     <>
       <main>
-        <div className="container">
-          {dice}
-        </div>
+        <div className="container">{dices}</div>
+        <button className="roll-btn" onClick={reRoll}>
+          Roll
+        </button>
       </main>
     </>
   );
