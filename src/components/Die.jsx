20ea09253c51d9a017d9b93b71182129d97263c1
Marion Jhon Agapay <marionagapay2001@gmail.com>
2025-07-30 21:28:16 +0800

    refactor: improve button accessibility with ARIA attributes

--- src/components/Die.jsx
+++ src/components/Die.jsx
@@ -1,10 +1,19 @@
 const Die = (props) => {
   const style = {
-    backgroundColor: props.isHeld === true ? "#59E391" : "#FFFFFF"
-  }
+    backgroundColor: props.isHeld === true ? "#59E391" : "#FFFFFF",
+  };
   return (
     <>
-        <button onClick={props.click} style={style}>{props.value}</button>
+      <button
+        onClick={props.click}
+        style={style}
+        aria-pressed={props.isHeld}
+        aria-label={`Die with value ${props.value}, ${
+          props.isHeld ? "Held" : "Not held"
+        }`}
+      >
+        {props.value}
+      </button>
     </>
   );
 };
