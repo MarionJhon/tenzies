6bc76ecb80d9560cda947ed83d5581813a970379
Marion Jhon Agapay <marionagapay2001@gmail.com>
2025-07-30 21:24:50 +0800

    feat: restart functionality if the game is won and also add a screen reader

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -5,16 +5,17 @@ import Die from "./Die";
 import "../App.css";
 
 function App() {
+  const [allNewDice, setAllNewDice] = useState(() => generateAllNewDice());
+
   //this function is to generate 10 new dice with a random number 1-6
-  const generateAllNewDice = () => {
+  function generateAllNewDice() {
+    console.log("Generate another dice")
     return Array.from({ length: 10 }, () => ({
       value: Math.ceil(Math.random() * 6),
       isHeld: false,
       id: nanoid(),
     }));
-  };
-
-  const [allNewDice, setAllNewDice] = useState(generateAllNewDice());
+  }
 
   const hold = (id) => {
     setAllNewDice((prevDice) =>
@@ -37,22 +38,36 @@ function App() {
   const reRoll = () => {
     setAllNewDice((prevRoll) =>
       prevRoll.map((roll) =>
-        roll.isHeld ? roll : {...roll, value: Math.ceil(Math.random() * 6)}
+        roll.isHeld ? roll : { ...roll, value: Math.ceil(Math.random() * 6) }
       )
     );
   };
 
-  const gameWon = allNewDice.every(die => die.isHeld) && allNewDice.every(die => die.value === allNewDice[0].value) ? true : false
+  const gameWon =
+    allNewDice.every((die) => die.isHeld) &&
+    allNewDice.every((die) => die.value === allNewDice[0].value)
+      ? true
+      : false;
+
+  const restart = () => {
+    gameWon && setAllNewDice(generateAllNewDice())
+  }
 
   return (
     <>
       <main>
-        {gameWon && <Confetti/>}
+        {gameWon && <Confetti />}
+        <div aria-live="polite" className="sr-only">
+          {gameWon && <p>Congratulations! You won! Press "New Game" to start again.</p>}
+        </div>
         <h1 className="title">Tenzies</h1>
-        <p className="instruction">Roll until all dice are the same. Click each die to freeze it at its current value between rolls</p>
+        <p className="instruction">
+          Roll until all dice are the same. Click each die to freeze it at its
+          current value between rolls
+        </p>
         <div className="container">{dices}</div>
-        <button className="roll-btn" onClick={reRoll}>
-          {gameWon ? 'New Game' : 'Roll'}
+        <button className="roll-btn" onClick={gameWon ? restart : reRoll}>
+          {gameWon ? "New Game" : "Roll"}
         </button>
       </main>
     </>
